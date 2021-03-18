fa8e2a7e93a082f92df24c58fe316b9bfb7d947c
Karina <krs_souza@yahoo.com.br>
2021-03-18 20:47:30 -0300

    mais um commit

--- Empregado.java
+++ Empregado.java
@@ -45,3 +45,28 @@ public class Empregado {
     }
 
 }
+
+    // adicionar à classe Empregado.java
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getComissao() {
+        return this.comissao;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+public double getBonus (){
+return this.bonus;
+}
